f092e57843dbfed1bfd52f9991978c24a039f26e
dmzlol <73185443+dmzlol@users.noreply.github.com>
2020-10-22 10:03:19 -0400

    starting work

---
+++ Script1.js
@@ -0,0 +1,6 @@
+// JavaScript source code
+
+
+
+// TechDegree Project 1
+
---
+++ js/script.js
@@ -0,0 +1,62 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+const quotes = [
+{
+	quote:  " I didn't know how many of them it was going to take to whip my ass, but I knew how many they were going to use. That's a handy piece of information to have, right there.",
+	source: "Ron White",
+	citation: "Blue Collar Comedy Tour",
+	year: 2003
+},
+{
+	quote: "When you're at the end of your rope, tie a knot and hold on.",
+	source: "Theodore Roosevelt",
+},
+{
+	quote: "To me this is not yelling. I am not yelling. I'm just passionate about my opinions and I want to tell you all of them before you start talking again.",
+	source: "Bill Burr",
+},
+{
+	quote: "The scariest moment is always just before you start.",
+	source: "Stephen King",
+	citation: "On Writing: A Memoir of the Craft",
+	year: 2000
+},
+{
+	quote: "Sometimes, making the wrong choice is better than making no choice. You have the courage to go forward, that is rare. A person who stands at the fork, unable to pick, will never get anywhere.",
+	source: "Terry Goodkind",
+	citation: "Wizard's First Rule",
+	year: 1994
+}
+
+]
+
+console.log(quotes);
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
